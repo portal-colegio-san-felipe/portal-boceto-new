454aeb2919c353a223ef86dc9ce740c4557b1038
portal-colegio-san-felipe <grupo1githubrepo2@outlook.com>
2026-05-03 22:07:43 +0000

    Prueba de cambios de texto mediante editor externo

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,9 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  server: {
+    // @ts-ignore
+    allowedHosts: process.env.TEMPO === "true" ? true : undefined,
+    host: process.env.TEMPO === "true" ? '0.0.0.0' : undefined,
+  },
 });
